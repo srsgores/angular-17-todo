d94a352727683f1bd8c689980166e1020ebb6dda
Sean Goresht <seangores@gmail.com>
2023-12-18 15:13:10 -0700

    feat: base styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import {CommonModule} from "@angular/common";
 import {RouterOutlet} from "@angular/router";
 import {NavigationComponent} from "./navigation/navigation.component";
 import {TodosManagerComponent} from "./todos-manager/todos-manager.component";
+import "the-new-css-reset/css/reset.css";
 
 @Component({
 	selector: "app-root",
